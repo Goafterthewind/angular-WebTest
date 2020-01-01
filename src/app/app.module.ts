2ed51f9a1f61d9f04937afd8d98604916dba50c5
Goafterthewind <2239765606@qq.com>
2020-01-01 11:05:02 +0800

    删除AppModule的路由文件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
@@ -14,7 +13,6 @@ import { ProductListComponent } from './product-list/product-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
     ])
