1fd5d10cf65230ae6aec08f0af594fbdb36b9ed6
Goafterthewind <2239765606@qq.com>
2020-01-07 22:26:57 +0800

    angular的表单
    1、首先要在ts文件中注入formBuilder服务，用来构建表单
    2、定义一个formGroup类型的属性，用来存储表单模型
    3、在构造函数中使用fb的group方法，分别对表单的字段进行定义，并把结果赋值给formGroup
    4、在模板文件中创建表单控件，使用formGroup属性和ts文件中的表单模型绑定
    5、在表单项中使用formControlName属性把表单中的字段和对应的输入值绑定
    6、在表单中使用（submit）事件绑定表单提交方法

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {  CartService } from '../cart.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -9,8 +10,11 @@ import {  CartService } from '../cart.service';
 export class CartComponent implements OnInit {
   items: any;
 
+  checkOutForm: FormGroup;
+
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private fb: FormBuilder
   ) {
     this.items = this.cartService.getItems();
     console.log(this.items);
@@ -19,6 +23,19 @@ export class CartComponent implements OnInit {
   ngOnInit() {
     const data = this.cartService.getShippingPrices();
     console.log(data);
+
+    this.checkOutForm = this.fb.group({
+      name: '',
+      address: ''
+    });
+  }
+
+  onSubmit(customerData) {
+    // Process checkout data here
+    console.warn('Your order has been submitted' + customerData);
+    this.items = this.cartService.clearCart(); // 清空购物车
+    console.log(this.items);
+    this.checkOutForm.reset(); // 重置表单
   }
 
 }
--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -19,12 +19,12 @@ export class ShippingComponent implements OnInit {
 
   getData() {
     // 不使用subscribe订阅，获取到的是一个流，通过异步管道（async）在模板文件中自动订阅
-    // this.shippingCosts = this.cartService.getShippingPrices();
+     this.shippingCosts = this.cartService.getShippingPrices();
 
     // 使用subscribe订阅
-    this.cartService.getShippingPrices().subscribe(res => {
-      this.shippingCosts = res;
-    });
+    // this.cartService.getShippingPrices().subscribe(res => {
+    //   this.shippingCosts = res;
+    // });
   }
 
 }
